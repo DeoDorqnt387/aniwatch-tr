0e93e909dbd928e7972d772c4f5101f858cb9224
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-08 00:15:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Gerekli izinleri ayarla ve komut dosyasını /usr/local/bin'e taşı
-sudo chmod +x aniwatch/aniwatch
-sudo mv aniwatch/aniwatch /usr/local/bin/aniwatch-tr
+sudo chmod +x aniwatch-tr/aniwatch-tr
+sudo mv aniwatch-tr/aniwatch-tr /usr/local/bin/aniwatch-tr
 
 # Proje klasörünü home dizinine taşı ve gerekli izinleri ayarla
-sudo mv aniwatch ~/.aniwatch_src
-sudo chmod 777 ~/.aniwatch_src
-sudo chmod 777 ~/.aniwatch_src/*
+sudo mv .aniwatch-tr ~/.aniwatch-tr
+sudo chmod 777 ~/.aniwatch-tr
+sudo chmod 777 ~/.aniwatch-tr/*
 
 # Sanal ortam oluştur ve bağımlılıkları yükle
 cd ~/.aniwatch_src && sudo python3 -m venv .venv
