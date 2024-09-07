d7249b6cb2a11397587ca9d37b418d564238ced0
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-08 00:35:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ cd "$(dirname "$0")"
 
 # Python sanal ortamını oluştur
 python3 -m venv .venv
+
+# Sanal ortamı aktif et
 source .venv/bin/activate
 
 # Bağımlılıkları yükle
@@ -12,6 +14,14 @@ pip install -r requirements.txt
 
 # Python dosyasına çalıştırma izni ver
 chmod +x main.py
+
+# Programı /usr/local/bin dizinine sembolik link oluştur
+cat << EOF | sudo tee /usr/local/bin/aniwatch-tr > /dev/null
+#!/bin/bash
+source $(pwd)/.venv/bin/activate
+exec python3 $(pwd)/main.py "\$@"
+EOF
+
 # Betiğe çalıştırma izni ver
 sudo chmod +x /usr/local/bin/aniwatch-tr
 
