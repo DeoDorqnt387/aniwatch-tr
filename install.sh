3e584ee620c70eaf3162458e2b691a8ade5a8921
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-08 00:26:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ sudo ln -sf "$(pwd)/main.py" /usr/local/bin/aniwatch-tr
 # Python sanal ortamını oluştur ve bağımlılıkları yükle
 python3 -m venv .venv
 source .venv/bin/activate
-pip install requests
-pip install inquirer
+pip install -r requirements.txt
 
 echo "Kurulum tamamlandı. 'aniwatch-tr' komutunu kullanarak programı çalıştırabilirsiniz."
