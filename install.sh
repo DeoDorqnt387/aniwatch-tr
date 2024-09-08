93a0e1eb47a9f0f344e7aaf89e38e4244fdadfa4
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-08 13:24:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 PROJECT_DIR="$HOME/.aniwatch-tr"
 
 # Proje dizinine gidip izinleri ayarlayalım
-chmod +x $PROJECT_DIR/main.py
+chmod +x $PROJECT_DIR/aniwatch-tr.py
 chmod 755 $PROJECT_DIR
 chmod 755 $PROJECT_DIR/*
 
@@ -12,12 +12,6 @@ chmod 755 $PROJECT_DIR/*
 cd $PROJECT_DIR
 python3 -m venv .venv
 .venv/bin/pip install requests inquirer
-
-# Sanal ortamın yolunu PATH değişkenine ekleyelim
-if ! grep -q 'export PATH="$HOME/.aniwatch-tr/.venv/bin:$PATH"' ~/.bashrc; then
-    echo 'export PATH="$HOME/.aniwatch-tr/.venv/bin:$PATH"' >> ~/.bashrc
-    source ~/.bashrc
-fi
 
 # Komut dosyasını sistem genelinde erişilebilir yapalım
 echo -e '#!/bin/bash\n'\
