c4700cb9da4faded9457b6799306885973c97cda
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-08 14:08:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-PROJECT_DIR="$HOME/.aniwatch-tr"
+sudo chmod +x aniwatch-tr/aniwatch-tr
 
-if [ -d "$PROJECT_DIR" ]; then
-    cd "$PROJECT_DIR" || { echo "Dizin '$PROJECT_DIR' mevcut değil."; exit 1; }
-
-    # Dosyaya çalıştırılabilir izinler verin
-    chmod +x "$PROJECT_DIR/aniwatch-tr"
-
-    # Sanal ortam oluşturun
-    python3 -m venv .venv
-
-    # Sanal ortamı aktive edin ve gerekli paketleri yükleyin
-    source .venv/bin/activate
-    pip install requests inquirer
-
-    echo "Kurulum tamamlandı."
-else
-    echo "Hata: '$PROJECT_DIR' dizini mevcut değil."
-    exit 1
-fi
+cd aniwatch-tr && sudo python3 -m venv .venv
+cd aniwatch-tr && sudo .venv/bin/pip3 install requests inquirer
