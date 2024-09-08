0b176d6fc3baef1648a5bb8aacf2191dea44a9d0
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-08 12:58:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,18 @@
 PROJECT_DIR="$HOME/.aniwatch-tr"
 
 # İzinleri ayarlayalım
-chmod +x $PROJECT_DIR/main.py  # Buraya çalıştırmak istediğiniz Python dosyasının yolunu ekleyin
-chmod 777 $PROJECT_DIR
-chmod 777 $PROJECT_DIR/*
+chmod +x $PROJECT_DIR/main.py
+chmod 755 $PROJECT_DIR
+chmod 755 $PROJECT_DIR/*
 
-# Python sanal ortamı oluşturalım ve gerekli paketleri yükleyelim
+# Python sanal ortamı oluşturulacak ve gerekli paketler yüklenecek
 cd $PROJECT_DIR
 python3 -m venv .venv
-.venv/bin/pip install requests inquirerpy
+.venv/bin/pip install requests inquirer
 
 # Komut dosyasını sistem genelinde erişilebilir yapalım
-echo -e '#!/bin/bash\n. '$PROJECT_DIR'/.venv/bin/python '$PROJECT_DIR'/main.py' | sudo tee /usr/local/bin/aniwatch-tr
+echo -e '#!/bin/bash\n'\
+'. '$PROJECT_DIR'/.venv/bin/python '$PROJECT_DIR'/main.py' | sudo tee /usr/local/bin/aniwatch-tr
 sudo chmod +x /usr/local/bin/aniwatch-tr
 
 echo "Kurulum tamamlandı. 'aniwatch-tr' komutunu kullanarak projeyi çalıştırabilirsiniz."
