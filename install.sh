fa6aa2d014bf7d377a895aa4136a961256b875fe
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-08 13:25:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Proje dizinini tanımlayalım
 PROJECT_DIR="$HOME/.aniwatch-tr"
 
-# Proje dizinine gidip izinleri ayarlayalım
 chmod +x $PROJECT_DIR/aniwatch-tr.py
 
-# Python sanal ortamını oluşturup gerekli paketleri yükleyelim
 cd $PROJECT_DIR
 python3 -m venv .venv
 .venv/bin/pip install requests inquirer
 
 sudo chmod +x /usr/local/bin/aniwatch-tr
-
-echo "Kurulum tamamlandı. 'aniwatch-tr' komutunu kullanarak projeyi çalıştırabilirsiniz."
