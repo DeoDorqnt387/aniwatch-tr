6bf093280ede57dc0185a86d46b4fc6f7d9d4d6f
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-08 03:20:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,33 +4,9 @@
 sudo chmod +x aniwatch-tr
 sudo mv aniwatch-tr /usr/local/bin/
 
-# Kaynak dizinini kontrol et
-if [ -d ~/aniwatch-tr ]; then
-  if [ -f ~/aniwatch-tr/main.py ]; then
-    sudo cp ~/aniwatch-tr/main.py /usr/local/bin/aniwatch-tr
-    sudo chmod +x /usr/local/bin/aniwatch-tr
-  else
-    echo "Dosya bulunamadı: ~/aniwatch-tr/main.py"
-    exit 1
-  fi
-else
-  echo "Dizin bulunamadı: ~/aniwatch-tr"
-  exit 1
-fi
+sudo cp -r ~/aniwatch-tr /usr/local/aniwatch-tr
+sudo chmod +x /usr/local/aniwatch-tr/main.py
 
-# PYTHONPATH ayarlamak
-export PYTHONPATH=$PYTHONPATH:~/aniwatch-tr
+pip install -r requirements.txt
 
-# Sanal ortamı oluşturup gerekli paketleri yüklemek
-if [ -d ~/.aniwatch-tr_src ]; then
-  cd ~/.aniwatch-tr_src || { echo "Dizin mevcut değil: ~/.aniwatch-tr_src"; exit 1; }
-
-  # Python sanal ortamını oluşturma
-  python3 -m venv .venv
-
-  # Paketleri yükleme
-  .venv/bin/pip install -r requirements.txt
-else
-  echo "Dizin mevcut değil: ~/.aniwatch-tr_src"
-  exit 1
-fi
+echo "Kurulum tamamlandı. Artık 'aniwatch-tr' komutunu kullanabilirsiniz."
