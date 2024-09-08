8796428c9c4d0a56742cc98e9c83e27f6c4a47c4
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-08 03:13:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,15 @@
 
 # İzinleri ayarlama ve dosyayı taşımak
 sudo chmod +x aniwatch-tr
-sudo mv aniwatch-tr /usr/local/bin
+sudo mv aniwatch-tr /usr/local/bin/
 
 # Kaynak dizinini taşımak ve izinlerini ayarlamak
 sudo cp ~/aniwatch-tr/main.py /usr/local/bin/aniwatch-tr
 sudo chmod +x /usr/local/bin/aniwatch-tr
 export PYTHONPATH=$PYTHONPATH:~/aniwatch-tr
+
 # Sanal ortamı oluşturup gerekli paketleri yüklemek
-cd ~/.aniwatch-tr_src
+cd ~/.aniwatch-tr_src || { echo "Dizin mevcut değil: ~/.aniwatch-tr_src"; exit 1; }
 
 # Python sanal ortamını oluşturma
 python3 -m venv .venv
