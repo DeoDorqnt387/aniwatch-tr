132ba065a70c194ba0a2dad615c3feb901d475a8
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-08 14:10:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 sudo chmod +x aniwatch-tr/aniwatch-tr
 
-cd aniwatch-tr && sudo python3 -m venv .venv
-cd aniwatch-tr && sudo .venv/bin/pip3 install requests inquirer
+cd aniwatch-tr || exit
+sudo python3 -m venv .venv
+.venv/bin/pip3 install requests inquirer
