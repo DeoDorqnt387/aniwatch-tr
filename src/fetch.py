405f49520e6c43f4a72170357739a26bc0198d49
Pandora <37597520+DeoDorqnt387@users.noreply.github.com>
2024-09-10 03:52:58 +0300

    Düzeltme fetch.py

--- src/fetch.py
+++ src/fetch.py
@@ -13,17 +13,47 @@ class fetch_data:
             {'name': item.get('name', 'No name field'), 'id': item.get('id', 'No ID field')}
             for item in data.get('results', [])
         ]
+    
+    def fetch_anime_seasons(self, selected_id):
+        url = f"https://www.mangacix.net/secure/related-videos?episode=1&season=1&titleId={selected_id}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            try:
+                json_data = response.json()
+
+                if "videos" in json_data:
+                    seasons = json_data["videos"][0]["title"]["seasons"]
+
+                    if isinstance(seasons, list):
+                        return list(range(len(seasons)))
+                    else:
+                        print("Expected 'seasons' to be a list, but got:", type(seasons))
+                        return []
+                else:
+                    print("Key 'videos' or 'seasons' not found in the JSON response")
+                    return []
+            except ValueError:
+                print("Invalid JSON response")
+                return []
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            return []
 
     def fetch_anime_eps(self, selected_id):
-        data_url = f"https://www.mangacix.net/secure/related-videos?episode=1&season=1&titleId={selected_id}"
-        response = requests.get(data_url)
-        response.raise_for_status()
-        data = response.json()
+        index_of_seasons = self.fetch_anime_seasons(selected_id)
 
         episodes = []
-        for item in data.get('videos', []):
-            episode_name = item.get('name', 'No name field')
-            episode_url = item.get('url', 'No URL field')
-            episodes.append({'name': episode_name, 'url': episode_url})
+        for index in index_of_seasons:
+            data_url = f"https://www.mangacix.net/secure/related-videos?episode=1&season={index + 1}&titleId={selected_id}"
+            response = requests.get(data_url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('videos', []):
+                episode_name = item.get('name', 'No name field')
+                episode_url = item.get('url', 'No URL field')
+                episode_thumbnail = item.get('thumbnail', 'No thumbnail field')
+                episodes.append({'name': episode_name, 'url': episode_url, 'thumbnail': episode_thumbnail})
 
         return episodes
