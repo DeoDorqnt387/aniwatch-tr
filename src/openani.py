6d5e7ab3ab9e2f43deb9ed0f647b3243a4422853
Pai1 <37597520+DeoDorqnt387@users.noreply.github.com>
2025-01-24 19:14:46 +0300

    Update openani.py

--- src/openani.py
+++ src/openani.py
@@ -1,11 +1,89 @@
-import subprocess, os, time
+import subprocess, os, time, requests, tools
 
+from urllib.parse import urlparse, parse_qs
 from InquirerPy import inquirer, prompt
 from InquirerPy.base.control import Choice
-
-## My Imports ##
-from fetch import FetchData_b
 from watch import *
+
+class FetchData_b:
+    def __init__(self):
+        # WEBSITE: https://openani.me/
+        self.base_url = "https://api.openani.me"
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+
+    def fetch_anime_search_data(self, query):
+        """Fetch anime search data based on the query."""
+        search_url = f"{self.base_url}/anime/search?q={query}"
+        data = self._get_json(search_url)
+        if data:
+            return [{'name': anime.get('english'), 'slug': anime.get('slug')} for anime in data]
+        return []
+
+    def fetch_anime_seasons_data(self, slug):
+        """Fetch anime season data based on the slug."""
+        url = f"{self.base_url}/anime/{slug}"
+        data = self._get_json(url)
+        if data:
+            seasons_count = data.get("numberOfSeasons", 0)
+            malID = data.get("malID")
+            type = data.get("type")
+
+            return seasons_count, slug, malID
+        return 0, slug, None
+
+    def fetch_anime_season_episodes(self, slug):
+        """Fetch all episodes for each season of the anime."""
+        season_count, slug, malID = self.fetch_anime_seasons_data(slug)
+        if not season_count:
+            print("No seasons found.")
+            return []
+
+        all_episodes = []
+        season_numbers = []
+
+        for season in range(1, season_count + 1):
+            url = f"{self.base_url}/anime/{slug}/season/{season}"
+            data = self._get_json(url)
+            if data:
+                episodes = data.get("season", {}).get("episodes", [])
+                season_number = data["season"]["season_number"]
+
+                all_episodes.extend(episodes)
+                season_numbers.extend([season_number] * len(episodes))
+
+        return sorted(
+            [(episode.get("name"), episode.get("episodeNumber"), season_number)
+             for episode, season_number in zip(all_episodes, season_numbers)],
+            key=lambda ep: (ep[2], ep[1])
+        )
+
+    def fetch_anime_episode_watch_api_url(self, slug, sel_ep, sel_seas):
+        """Fetch the watch URL for a specific episode."""
+        if not slug:
+            print("No slug provided.")
+            return None
+
+        url = f"{self.base_url}/anime/{slug}/season/{sel_seas}/episode/{sel_ep}"
+        data = self._get_json(url)
+        if data and "episodeData" in data:
+            for index in range(3, -1, -1):
+                try:
+                    return data["episodeData"]["files"][index]["file"]
+                except (IndexError, KeyError):
+                    continue
+        return None
+
+    def _get_json(self, url):
+        """Fetch JSON data from the specified URL."""
+        try:
+            response = requests.get(url, headers=self.headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            print(f"Error fetching JSON from {url}: {e}")
+            return None
 
 class Openani:
     def __init__(self):
@@ -16,46 +94,7 @@ class Openani:
         self.episode_index = 0
         self.selected_name = ""
         self.current_anime_name = ""
-        self.current_episode_index = 0
-
-    def play_current_episode(self, quality=None):
-        """Şu anki Bölümü Oynat"""
-        if self.current_episode_index is not None:
-            self.play_episode(self.current_episode_index)
-        else:
-            print("Bölüm Seçilmedi!")
-            time.sleep(0.8)
-
-    def next_episode(self):
-        """Siradaki Bölüme Git"""
-        if self.current_episode_index is not None and self.current_episode_index < len(self.episodes) -1:
-            self.current_episode_index += 1
-        else:
-            print("Sonraki Bölüm Yok!")
-            time.sleep(0.8)
-    
-    def previous_episode(self):
-        """Önceki Bölüme Git"""
-        if self.current_episode_index is not None and self.current_episode_index > 0:
-            self.current_episode_index -= 1
-        else:
-            print("Önceki Bölüm Yok!")
-            time.sleep(0.8)
-
-    def clear_screen(self):
-        """Terminal Ekranını Temizle"""
-        os.system('cls' if os.name == 'nt' else 'clear')
-
-    def invalid_option(self):
-        """Geçersiz veri"""
-        print("Geçersiz bir seçenek girdiniz. Lütfen geçerli bir seçenek giriniz.")
-        time.sleep(1)
-
-    def exit_app(self):
-        """Çık"""
-        print("Çıkış yapılıyor...")
-        time.sleep(0.6)
-        exit()
+        self.current_episode_index = None
 
     def select_ep(self):
         """Bölüm Seç"""
@@ -83,7 +122,7 @@ class Openani:
     
     def display_menu(self):
         """Ana Menü Gösterimi"""
-        self.clear_screen() 
+        tools.clear_screen() 
         print(f"\033[33mOynatılıyor\033[0m: {self.current_anime_name} (tr-altyazılı) | {self.current_episode_index + 1}/{len(self.episodes)}")
         
         selected_option = prompt([{
@@ -100,16 +139,16 @@ class Openani:
     def handle_menu_option(self, option):
         """Menu Seçim"""
         actions = {
-            'Şu anki Bölümü Oynat': self.play_current_episode,
-            'Sonraki Bölüm': self.next_episode,
-            'Önceki Bölüm': self.previous_episode,
-            'Bölüm Seç': self.select_ep,
+            'Şu anki Bölümü Oynat': lambda: tools.play_current_episode(self),
+            'Sonraki Bölüm': lambda: tools.next_episode(self),
+            'Önceki Bölüm': lambda: tools.previous_episode(self),
+            'Bölüm Seç': lambda: tools.select_ep(self),
             'Anime Ara': self.srch_anime,
             'Bölüm İndir': self.download_eps,
-            'Çık': self.exit_app,
+            'Çık': tools.exit_app,
         }
         
-        actions.get(option, self.invalid_option)() if isinstance(option, str) else self.invalid_option()
+        actions.get(option, tools.invalid_option)() if isinstance(option, str) else tools.invalid_option()
 
     def srch_anime(self):
         """Anime Arat"""
@@ -131,7 +170,7 @@ class Openani:
         self.episodes = self.ftch_dt_b.fetch_anime_season_episodes(self.slug)
 
         while True:
-            self.clear_screen()
+            tools.clear_screen()
             self.handle_menu_option(self.display_menu())
 
         #return self.current_anime_name, self.slug
@@ -194,6 +233,3 @@ class Openani:
 
         
 
-if __name__ == "__main__":
-    open = Openani()
-    open.srch_anime()
