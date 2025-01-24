b183c42a692fbe608be7ffccf491ebf2c3edeaf5
Pai1 <37597520+DeoDorqnt387@users.noreply.github.com>
2025-01-24 19:16:42 +0300

    Revert "gereksiz"

--- src/main.py
+++ src/main.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python3
-from animeci import animecix
-from openani import Openani
-import tools
-
-def main():
-    app = animecix()
-    openani = Openani()
-    selection = tools.display_website_selection_thing()
-    if selection == "AnimeciX (ID: 856)":
-        app.srch_anime()
-    else:
-        openani.srch_anime()
-
+from animeci import main
 
 if __name__ == "__main__":
     main()
