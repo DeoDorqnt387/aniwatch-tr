90226b1286b8e2b9aa23305d268029fcaaac77b1
Pai1 <37597520+DeoDorqnt387@users.noreply.github.com>
2025-03-01 00:24:26 +0300

    Update watch.py

--- src/watch.py
+++ src/watch.py
@@ -1,18 +1,8 @@
 import subprocess
 
-def open_with_video_player(url, use_vlc=False):
+def open_with_video_player(url):
     """Video Oynatıcı"""
-    if use_vlc:
-        try:
-            if not isinstance(url, str):
-                raise ValueError("Url must be a string.")
-            subprocess.run(['vlc', '-v', "--quiet", '--fullscreen', url], check=True)
-        except subprocess.CalledProcessError as e:
-            print("Oynatılırken Hata Oluştu!", e)
-    else:
-        try:
-            subprocess.run(['mpv', '--fullscreen', url], check=True)
-        except subprocess.CalledProcessError as e:
-            print("Oynatılırken Hata Oluştu!", e)
-
-
+    try:
+        subprocess.run(['mpv', '--fullscreen', url], check=True)
+    except subprocess.CalledProcessError as e:
+        print("Oynatılırken Hata Oluştu!", e)
