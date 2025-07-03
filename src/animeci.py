c3d09fb65638349ceb942c7d5d412289fa65fa4b
Pai1 <37597520+DeoDorqnt387@users.noreply.github.com>
2025-07-03 23:25:47 +0300

    anm.cx to animecix.tv

--- src/animeci.py
+++ src/animeci.py
@@ -8,8 +8,8 @@ from watch import *
 
 class anifetch:
     def __init__(self):
-        # WEBSITE: https://anm.cx/
-        self.base_url = "https://anm.cx/"
+        # WEBSITE: https://animecix.tv/
+        self.base_url = "https://animecix.tv/"
         self.video_players = ["tau-video.xyz", "sibnet"]
         self.headers = {
             'Accept': 'application/json',
